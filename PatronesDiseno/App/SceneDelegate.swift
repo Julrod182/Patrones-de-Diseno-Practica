022d900f0161ff72291659a7f024274c4c122d55
Julio Rodriguez <julrod182@gmail.com>
2022-07-21 13:50:21 -0500

    Inyección de dependencias

--- PatronesDiseno/App/SceneDelegate.swift
+++ PatronesDiseno/App/SceneDelegate.swift
@@ -10,13 +10,31 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-
+    
+    private let splashStoryboardName = "Splash"
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        let window = UIWindow(windowScene: windowScene)
+        
+        let splashStoryboard = UIStoryboard(name: splashStoryboardName, bundle: nil)
+        
+        guard let destination = splashStoryboard.instantiateInitialViewController() as? SplashViewController else {return}
+        
+        // Inyección del viewModel: Se pude crear un viewmodel diferente que cumpla con el protocolo
+        destination.viewModel = SplashViewModel(viewDelegate: destination)
+        
+        //Creación del navigation controller
+        let navigationController = UINavigationController(rootViewController: destination)
+        window.rootViewController = navigationController
+        
+        //Asigna el valor de la vista y se hace visible
+        self.window = window
+        window.makeKeyAndVisible()
+        
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
